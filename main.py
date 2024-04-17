3d6df59c5134d552ff20ed29889bb2648d8394cc
RyanSingh <ryan.sohi84@gmail.com>
2024-04-17 13:50:14 +0200

    ja

--- main.py
+++ main.py
@@ -17,14 +17,17 @@ SCREEN_HEIGHT = 600
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pg.display.set_caption("Pong")
 
+# player 1 variebls
 player_x_1 = SCREEN_WIDTH - 30
 player_y_1 = SCREEN_HEIGHT // 2
 player_1_score = 0
 
+# player 2 varibles
 player_x_2 = 30
 player_y_2 = SCREEN_HEIGHT // 2
 player_2_score = 0
 
+# ball variebels
 ball_x = SCREEN_WIDTH // 2
 ball_y = SCREEN_HEIGHT // 2
 ball_radius = 10
@@ -33,14 +36,14 @@ ball_speed = 1
 ball_y_speed = 0.2 * ball_speed
 ball_x_speed = 0.2 * ball_speed
 
-
+# function to have the balls speed fliped if it hits the player
 def update_speed():
     global ball_x_speed, ball_y_speed, ball_speed
-    ball_speed += 0.1
+    ball_speed += 1
     ball_x_speed = 0.1 * ball_speed * (-1 if ball_x_speed < 0 else 1)  # maintain direction when speeding up
     ball_y_speed = 0.1 * ball_speed * (-1 if ball_y_speed < 0 else 1)
 
-
+# score
 font = pg.font.Font(None, 36)
 textRect_1 = None
 textRect_2 = None
@@ -62,12 +65,15 @@ while running:
             elif event.key == K_ESCAPE:
                 running = False
 
+    # the balls force
     ball_y += ball_y_speed
     ball_x += ball_x_speed
 
+    # ball hits the flore or roof
     if ball_y <= 0 + ball_radius or ball_y >= SCREEN_HEIGHT - ball_radius:
         ball_y_speed *= -1
 
+    # player 2 scores
     if ball_x >= SCREEN_WIDTH - ball_radius:
         player_2_score += 1
         ball_x, ball_y = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
@@ -75,6 +81,7 @@ while running:
         ball_x_speed = 0.2 * ball_speed  # reset speed to right
         ball_y_speed = 0.2 * ball_speed
 
+    # player 1 scores
     if ball_x <= 0 + ball_radius:
         player_1_score += 1
         ball_x, ball_y = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
@@ -92,14 +99,18 @@ while running:
         ball_x_speed *= -1
         update_speed()
 
+    # fill the screen white
     screen.fill((255, 255, 255))
+    # display score
     text_score_1 = font.render(f"{player_1_score}", True, (0, 0, 0), (255, 255, 255))
     text_score_2 = font.render(f"{player_2_score}", True, (0, 0, 0), (255, 255, 255))
     textRect_1 = text_score_1.get_rect(center=(SCREEN_WIDTH // 2 + 30, 50))
     textRect_2 = text_score_2.get_rect(center=(SCREEN_WIDTH // 2 - 30, 50))
     screen.blit(text_score_1, textRect_1)
     screen.blit(text_score_2, textRect_2)
+    # display the ball
     pg.draw.circle(screen, (0, 0, 0), (ball_x, ball_y), ball_radius)
+    # display the players
     pg.draw.line(screen, (0, 0, 0), (player_x_2, player_y_2), (player_x_2, player_y_2 - 75), 10)
     pg.draw.line(screen, (0, 0, 0), (player_x_1, player_y_1), (player_x_1, player_y_1 - 75), 10)
     pg.display.flip()
